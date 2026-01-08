ea6306c9270eee01176a6247c26e3be07d37f292
Bilawal-Rizvi <shahb7404@gmail.com>
2026-01-08 22:08:50 +0500

    Hel

--- routes/shopStokeRoutes.js
+++ routes/shopStokeRoutes.js
@@ -4,11 +4,12 @@
     const protect = require("../middleware/authMiddleware");
 
     // 🔐 All routes protected
+     router.get("/search", protect, shopController.search);
     router.get("", protect, shopController.getAll);
     router.get("/:id", protect, shopController.getById);
     router.post("/", protect, shopController.create);
     router.put("/:id", protect, shopController.update);
     router.delete("/:id", protect, shopController.delete);
-    router.get("/search", protect, shopController.search);
+   
 
     module.exports = router;
